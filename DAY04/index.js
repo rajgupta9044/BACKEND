44d3962482c651786e042fbea46fddbda17247be
Raj Gupta <rajgpt9044@gmail.com>
2026-02-07 05:06:47 +0530

    DAY 5

--- DAY04/index.js
+++ DAY04/index.js
@@ -1,8 +1,29 @@
+//CREATING A SERVER 
+
+
 const http =require('http');
 
 const server =http.createServer((req,res)=>{
 
-    res.end("Helllo Everyone ")
+
+    //display the message at port number 4000
+    // res.end("Helllo Everyone ")
+
+
+    if(req.url==='/contact'){
+        res.end("this is the contact page");
+    }
+
+    else if (req.url==="/about"){
+        res.end("this is the about page");
+    }
+
+    else{
+        res.end("page not found");
+    }
+
+
+    
 });
 
 server.listen(4000,()=>{
---
+++ DAY05/index.js
@@ -0,0 +1,8 @@
+//MicroService and Monolithic
+
+// In microservice we have different servers to handle differnt 
+// things.
+// Due to this scalability is quite easy 
+
+
+// But in monothilic scalability is very hard 
